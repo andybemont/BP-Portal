9d2865ef85870cef1eaff98e4c1494ca0d66022e
Andy Bemont <andy.bemont@calero.com>
2024-11-27 10:06:47 -0500

    build fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import { Metadata } from "next";
-import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   title: {
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import { authenticate } from '@/app/lib/actions';
-import { lusitana } from '@/app/ui/fonts';
+import { authenticate } from "@/app/lib/helpers";
+import { lusitana } from "@/app/ui/fonts";
 import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
-} from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
-import { useActionState } from 'react';
+} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { Button } from "./button";
+import { useActionState } from "react";
 
 export default function LoginForm() {
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
-    undefined,
+    undefined
   );
 
   return (
