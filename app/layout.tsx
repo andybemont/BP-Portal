4264bbd0a82cae737fa77f7c35bb1e7be8a8a9fa
Andy Bemont <andy.bemont@calero.com>
2024-12-05 10:55:41 -0500

    external links

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,11 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Acme Dashboard",
-    default: "Acme Dashboard",
+    template: "Bemont Photo Portal",
+    default: "Bemont Photo Portal",
   },
-  description: "The official Next.js Learn Dashboard built with App Router.",
-  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+  description:
+    "Helping Exceptional Photographers Do Exception Wedding Photography.",
 };
 export default function RootLayout({
   children,
